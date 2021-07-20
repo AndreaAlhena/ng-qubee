28d5b284670702aae19506d67d10852dd4ac7507
Andrea Tantimonaco <learning@andreatantimonaco.me>
2021-07-20 23:41:30 +0200

    Fixed dependency injection

--- src/lib/ng-qubee.module.ts
+++ src/lib/ng-qubee.module.ts
@@ -1,6 +1,7 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { NgQubeeService } from './ng-qubee.service';
 import { IConfig } from './interfaces/config.interface';
+import { IPaginationConfig } from './interfaces/pagination-config.interface';
 import { PaginationService } from './services/pagination.service';
 import { StoreService } from './services/store.service';
 
@@ -15,11 +16,11 @@ import { StoreService } from './services/store.service';
 export class NgQubeeModule {
   private static _config: IConfig = {};
 
-  public static forRoot(config: IConfig): ModuleWithProviders<NgQubeeModule> {
+  public static forRoot(config: IConfig = {}): ModuleWithProviders<NgQubeeModule> {
     return {
       ngModule: NgQubeeModule,
       providers: [{
-        deps: [Store],
+        deps: [StoreService],
         provide: NgQubeeService,
         useFactory: (store: StoreService) =>
           new NgQubeeService(store, Object.assign({}, this._config.request, config.request))
