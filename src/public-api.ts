d5c55ace3654109fccae2cb908bffc585dc81456
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2024-02-08 20:36:38 +0100

    Public API exposes provide function

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,7 +2,8 @@
  * Public API Surface of angular-query-builder
  */
 
-export * from './lib/ng-qubee.service';
-export * from './lib/ng-qubee.module';
 export * from './lib/models/paginated-collection';
+export * from './lib/ng-qubee.module';
+export * from './lib/ng-qubee.service';
+export * from './lib/provide-ngqubee';
 export * from './lib/services/pagination.service';
