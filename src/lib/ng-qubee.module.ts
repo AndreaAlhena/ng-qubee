f4beaca70278c92bc956773a367e1aa4566a7d9f
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2024-02-14 20:08:09 +0100

    NestService provider

--- src/lib/ng-qubee.module.ts
+++ src/lib/ng-qubee.module.ts
@@ -2,14 +2,14 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { NgQubeeService } from './ng-qubee.service';
 import { IConfig } from './interfaces/config.interface';
 import { PaginationService } from './services/pagination.service';
-import { StoreService } from './services/store.service';
+import { NestService } from './services/nest.service';
 
 // @dynamic
 @NgModule({
   providers: [{
-    deps: [StoreService],
+    deps: [NestService],
     provide: NgQubeeService,
-    useFactory: (store: StoreService) => new NgQubeeService(store, {})
+    useFactory: (nestService: NestService) => new NgQubeeService(nestService, {})
   }]
 })
 export class NgQubeeModule {
@@ -17,17 +17,17 @@ export class NgQubeeModule {
     return {
       ngModule: NgQubeeModule,
       providers: [
+        NestService,
         {
-          deps: [StoreService],
+          deps: [NestService],
           provide: NgQubeeService,
-          useFactory: (store: StoreService) =>
-            new NgQubeeService(store, Object.assign({}, config.request))
+          useFactory: (nestService: NestService) =>
+            new NgQubeeService(nestService, Object.assign({}, config.request))
         }, {
           provide: PaginationService,
           useFactory: () =>
             new PaginationService(Object.assign({}, config.response))
-        },
-        StoreService
+        }
       ]
     };
   }
