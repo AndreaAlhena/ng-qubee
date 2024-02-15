e4f598895eed6a5e09bed980d590b53ad15e0a42
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2024-02-15 12:30:16 +0100

    Public api export path

--- src/public-api.ts
+++ src/public-api.ts
@@ -4,6 +4,6 @@
 
 export * from './lib/models/paginated-collection';
 export * from './lib/ng-qubee.module';
-export * from './lib/ng-qubee.service';
 export * from './lib/provide-ngqubee';
+export * from './lib/services/ng-qubee.service';
 export * from './lib/services/pagination.service';
