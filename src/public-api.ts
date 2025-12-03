c64b0a4d7a253bcf1c04bc16c77a27996406d560
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2025-12-03 18:44:37 +0100

    feat: Update public api

--- src/public-api.ts
+++ src/public-api.ts
@@ -7,3 +7,10 @@ export * from './lib/ng-qubee.module';
 export * from './lib/provide-ngqubee';
 export * from './lib/services/ng-qubee.service';
 export * from './lib/services/pagination.service';
+
+// Error classes
+export * from './lib/errors/invalid-model-name.error';
+export * from './lib/errors/invalid-page-number.error';
+export * from './lib/errors/invalid-limit.error';
+export * from './lib/errors/key-not-found.error';
+export * from './lib/errors/unselectable-model.error';
