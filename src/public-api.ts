5021bb29f43b8487a04473f55164b76ec3134b1a
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2025-12-06 20:03:26 +0100

    feat: Export interfaces and SortEnum

--- src/public-api.ts
+++ src/public-api.ts
@@ -8,9 +8,23 @@ export * from './lib/provide-ngqubee';
 export * from './lib/services/ng-qubee.service';
 export * from './lib/services/pagination.service';
 
+// Enums
+export * from './lib/enums/sort.enum';
+
 // Error classes
+export * from './lib/errors/invalid-limit.error';
 export * from './lib/errors/invalid-model-name.error';
 export * from './lib/errors/invalid-page-number.error';
-export * from './lib/errors/invalid-limit.error';
 export * from './lib/errors/key-not-found.error';
 export * from './lib/errors/unselectable-model.error';
+
+// Interfaces
+export * from './lib/interfaces/config.interface';
+export * from './lib/interfaces/fields.interface';
+export * from './lib/interfaces/filters.interface';
+export * from './lib/interfaces/nest-state.interface';
+export * from './lib/interfaces/page.interface';
+export * from './lib/interfaces/paginated-object.interface';
+export * from './lib/interfaces/pagination-config.interface';
+export * from './lib/interfaces/query-builder-config.interface';
+export * from './lib/interfaces/sort.interface';
